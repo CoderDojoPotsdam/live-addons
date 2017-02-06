8503c4e58dbe3ccb8f4d87a59ebd520e2732b9f9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-06 12:20:47 -0800

    added new iso with hash

--- 01_setup.sh
+++ 01_setup.sh
@@ -4,7 +4,8 @@ cd "`dirname \"$0\"`"
 
 sudo apt-get -y -qq install git
 
-url="https://github.com/CodersOS/live-addon-maker.git"
+url="http://cdimage.ubuntu.com/ubuntu-mate/releases/16.04.1/release/ubuntu-mate-16.04.1-desktop-amd64.iso"
+expected_hash="d17ad97753c756513e1c3add89ce6fa2f0db8c2fba690408a1e5eabc8e01311b"
 
 echo "Checking live-addon-maker"
 if [ -d "live-addon-maker" ]; then
@@ -21,6 +22,15 @@ echo "Looking for ubuntu at $url"
   cd "live-addon-maker"
   wget -c "http://de.releases.ubuntu.com/16.10/ubuntu-16.10-desktop-amd64.iso"
 )
+
+iso_hash="`sha256sum test.sh | grep -oE '^\S+'`"
+
+[ -z "$expected_hash" ] || [ "$expected_hash" == "$iso_hash" ] || {
+  echo "SHA256 hashes do not match."
+  echo "Expected: '$expected_hash'"
+  echo "     Got: '$iso_hash'"
+  exit 1
+}
 
 echo "Deleting examples"
 (
