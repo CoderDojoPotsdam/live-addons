21cad3981a85aaaa97ee381db0436c15ec36313a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-06 01:59:38 -0800

    faster clone with logs

--- 01_setup.sh
+++ 01_setup.sh
@@ -6,19 +6,23 @@ sudo apt-get -y -qq install git
 
 url="https://github.com/CodersOS/live-addon-maker.git"
 
+echo "Checking live-addon-maker"
 if [ -d "live-addon-maker" ]; then
   (
     cd "live-addon-maker"
     git pull
   )
 else
-  git clone "$url"
+  git clone --depth=1 "$url"
 fi
 
+echo "Looking for ubuntu at $url"
 (
   cd "live-addon-maker"
   wget -c "http://de.releases.ubuntu.com/16.10/ubuntu-16.10-desktop-amd64.iso"
 )
+
+echo "Deleting examples"
 (
   cd "live-addon-maker/examples"
   rm -f *.squashfs
