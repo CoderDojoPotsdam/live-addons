a3dd45f861cf065062fb801cb8d4524169d7626a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-07 10:07:16 -0800

    waiting

--- addon-desktop.sh
+++ addon-desktop.sh
@@ -5,11 +5,10 @@ cd "`dirname \"$0\"`"
 (
   mkdir -p files/icons
   cd files/icons
-  wget -c 'https://github.com/CoderDojoPotsdam/organize/raw/master/logo/logo-512.png'
+  wget -qc 'https://github.com/CoderDojoPotsdam/organize/raw/master/logo/logo-512.png'
 )
 
 sudo live-addon-maker/make-addon.sh ./link.iso z-desktop.squashfs \
-         -c 'echo /home/ubuntu/Desktop/* > /home/ubuntu/Desktop/.remove'
          -a 'files/Desktop/' '/home/ubuntu/Desktop/' \
          -a 'files/icons' '/opt/icons' \
-         -s 'clean-desktop' 'rm -f `cat /home/ubuntu/Desktop/.remove`'
+         -s 'clean-desktop' 'sleep 4; rm -f examples.desktop ubiquity.desktop'
