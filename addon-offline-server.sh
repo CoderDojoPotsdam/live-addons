d8abe87227505fe844ff96d9e55084a17fe21672
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-28 04:34:39 -0800

    added offline server

---
+++ addon-offline-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+
+modules="startup http-server lightbot blockly-games snap"
+install_url="https://raw.githubusercontent.com/cdpoffline/offline-material/master/bin/download_and_install.sh"
+
+sudo live-addon-maker/make-addon.sh ./link.iso z-offline-server.squashfs \
+         -C "sed -i=.live-addon 's/restricted$/restricted multiverse universe/' /etc/apt/sources.list" \
+         -C "apt-get update" \
+         -c "cd /opt && wget -O- '$install_url' | bash" \
+         -c "/opt/offline-material/bin/modules.sh -a $modules overview; /opt/offline-material/bin/modules.sh -u overview"
